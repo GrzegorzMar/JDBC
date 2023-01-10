6d75565a966bd0f8aeb55b6b641b71d3dcf80f55
GrzegorzMar <grzegorz.maruszewski@gmail.com>
2023-01-10 15:13:13 +0100

    commit 1

---
+++ src/main/java/jdbc/Main.java
@@ -0,0 +1,48 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException{
+        //create database books;
+        //Wersja dla MySql
+        DriverManager.getConnection("jdbc:mysql://localhost:3306/books","root","12345678");
+        //Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/testdb",
+        //"postgres", "MyPassword123");
+        //tabela na ksiazki:
+        String createTableSql = """
+                CREATE TABLE books (
+                id int AUTO_INCREMENT PRIMARY KEY,
+                title varchar(255) NOT NULL,
+                author varchar(255) NOT NULL,
+                pages int NOT NULL
+                );
+                """;
+
+        //connection.createStatement().execute(createTableSql);
+
+        // "jdbc:mysql://localhost:3306/sonoo","root","root"
+
+
+        /*Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/testdb",
+                "postgres", "MyPassword123");
+        connection.createStatement().execute("create table test(num int);");*/
+
+
+    }
+}
+
+//Bazy danych:
+// SQL / NoSQL (np. MongoDB)
+
+//1. Samodzielny SQL
+//2. JDBC - java wysyła zapytania SQL
+//zainstalowana i odpalona baza
+//sterownik (maven)
+//-miejsce na pisanie kodu - main?
+//Uzyskujemy Connection przez DriverManager
+//uwaga SQLException
+//3. Hibernate - zapisujemy OBIEKTY w bazie danych (ORM)
+//4. Spring JPA - automatyzacja pracy z Hibernate przy Spring
---
+++ src/main/java/movies/Menu.java
@@ -0,0 +1,93 @@
+package movies;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+//menu
+public class Menu {
+    private boolean running = true;
+    private List<Movie> movies = new ArrayList<>();
+
+    public void startMenu() {
+        do {
+            menuAction();
+        } while (running);
+    }
+
+    private void menuAction() {
+        showOptions();
+        int input = readDecision();
+        executeOption(input);
+    }
+
+    private void showOptions() {
+        System.out.println("""
+                Wybierz jedną z opcji:
+                1. Dodaj nowy film
+                2. Wyświetl filmy
+                3. Koniec""");
+    }
+
+    private int readDecision() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    private void executeOption(int input) {
+        switch (input) {
+            case 1:
+                addMovie();
+                break;
+            case 2:
+                showMovies();
+                break;
+            case 3:
+                end();
+                break;
+        }
+    }
+
+    private void addMovie() {
+        Movie movie = readMovieData();
+        save(movie);
+    }
+
+    private Movie readMovieData() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj tytuł:");
+        String title = scanner.nextLine();
+        System.out.print("Podaj rok premiery:");
+        int premiereYear = scanner.nextInt();
+        if(premiereYear<1800 || premiereYear>2100){
+            System.out.println("Podano nierealną datę premiery. " +
+                    "Powinien być przedział: 1800 - 2100");
+            return readMovieData();
+        }
+//        scanner = new Scanner(System.in);
+        scanner.nextLine();
+        System.out.print("Podaj gatunek:");
+        String genre = scanner.nextLine();
+        System.out.print("Podaj ocenę (1-5):");
+        int rate = scanner.nextInt();
+        return new Movie(title, premiereYear, genre, rate);
+    }
+
+    private void showMovies() {
+        for (Movie movie : movies) {
+            System.out.println(movie);
+        }
+    }
+
+    private void save(Movie movie) {
+        movies.add(movie);
+    }
+
+    private void end() {
+        System.out.println("Koniec");
+        running = false;
+    }
+
+
+    //każda metoda powinna operować na jednym poziomie abstrakcji
+}
